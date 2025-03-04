def22acfa3744ff2a7c87b828d7162f9f40f3d54
Parin <parinprojects@gmail.com>
2025-03-04 11:22:07 +0700

    fix 1

--- ex-1.js
+++ ex-1.js
@@ -1,14 +1,14 @@
 // Exercise #1: For Each Function
 
 function forEach(array, operation) {
-  // Start coding here
+  for (let i = 0; i < array.length; i++) {
+    operation(array[i], array); // เรียกใช้ operation กับค่าแต่ละตัวใน array
+  }
 }
-
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
-
 forEach(employeeSalaries, (salary) => {
   newEmployeeSalaries.push(salary + 5000);
 });
