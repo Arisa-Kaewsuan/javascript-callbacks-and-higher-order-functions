d3d5f0f33a74b32bbc3546f87834469d75a0bd07
Chayanon-Pond <chayanon.kmutnb@gmail.com>
2025-03-06 00:15:17 +0700

    feat:pond

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,32 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let i = 0; i < array.length ; i++) {
+    operation(array[i])
+  }
+
+  
 }
+
+function addSalaries (salary) {
+  
+    newEmployeeSalaries.push(salary+ 5000)
+
+}
+
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
+// EmployeeSalaries(employeeSalaries)
 
 // Using `forEach` function here
-
+forEach(employeeSalaries, addSalaries)
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+
+
+
+  
+
+
+
+
